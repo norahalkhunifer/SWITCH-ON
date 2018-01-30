138db09499925b5ab8dfa9a503b864d3d64bb812
Norah <alnoor0416@gmail.com>
2018-01-30 19:08:20 +0300

    level +worlds classes
    i went to get hot path

---
+++ Assets/worlds/Level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Level : MonoBehaviour {
+	 
+
+	public Text leveltitle;
+		public int state;
+	GameObject lightining;
+
+}
---
+++ Assets/worlds/WorldManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class WorldManager : MonoBehaviour {
+
+
+	[System.Serializable]
+	public class Level
+	{
+		public string leveltitle;
+		public int state;
+		public bool isintractible;
+		public Button.ButtonClickedEvent onclick;
+	}
+	public List<Level> levels;
+	public GameObject levelButton;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
