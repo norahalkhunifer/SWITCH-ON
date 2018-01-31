69ddcffff79aa59c9b7980ee741c77e24500ff44
Norah <alnoor0416@gmail.com>
2018-01-31 20:06:23 +0300

    doors scenes +sliders
    i went to the gym

--- Assets/worlds/WorldManager.cs
+++ Assets/worlds/WorldManager.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System.Collections;
+
 
 public class WorldManager : MonoBehaviour {
+	bool nextavi=true;
 
 	int state ;
 	[System.Serializable]
@@ -15,6 +18,7 @@ public class WorldManager : MonoBehaviour {
 		public bool isintractible;
 		public Button.ButtonClickedEvent onclick;
 	}
+	//Level +world lists
 	//public List<Level> levels;
 	//public GameObject levelButton;
 
@@ -26,5 +30,42 @@ public class WorldManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	//animate x pos 
+	//slide to next world
+	public void next(Animator anim){
+
+		if (nextavi) {
+			anim.SetBool ("thereisnext", true);
+			nextavi = false;
+		}
+	}
+	//slide to previos world
+	public void previos(Animator anim){
+
+		if (!nextavi) {
+			anim.SetBool ("thereisnext", false);
+
+			nextavi = true;
+		}
+	}
+	//animate y+x pos
+	//slide to setting panel
+	public void opensettings(Animator anim){
+		anim.SetBool ("settings", true);
+
+	}
+	public void closesettings(Animator anim){
+		anim.SetBool ("settings", false);
+
+	}
+
+	//go to level(dialog) scenes or give close feedback
+	public void gotolevel(string level){
+		//check if close or open!
+
+
+		Application.LoadLevel (level);
 	}
 }
