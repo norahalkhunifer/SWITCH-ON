21bec4b80fd21d35c34272d3575e7ad7be620f7e
Norah <alnoor0416@gmail.com>
2018-02-05 17:15:58 +0300

    update

---
+++ Assets/level3/level3manger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level3manger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void home(){
+
+		Application.LoadLevel("world");
+
+	}
+	public void open(){
+
+
+	}
+	public void close(){
+
+
+	}
+
+}
