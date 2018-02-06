b2f70671d232ae38faff34b3bc8363f40ed3fe61
renadsultan <32498605+renadsultan@users.noreply.github.com>
2018-02-06 22:59:26 +0300

    email

---
+++ Assets/worlds/settingManger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class settingManger : MonoBehaviour {
+
+	public void ContactUs()
+	{
+		string email = "renadalmalki2@gmail.com"; 
+		string subject = MyEscapeURL ("SUGGESTION"); 
+		string body = MyEscapeURL ("Please enter your message");
+
+		Application.OpenURL ("mailto:" + email + "?subject=" + subject + "&body=" + body); //open iphone mail 
+	}
+
+	string MyEscapeURL(string url){
+
+		return WWW.EscapeURL (url).Replace ("+", "%20");
+	}
+	 }
+
