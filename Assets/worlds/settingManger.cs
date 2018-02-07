bff1aa8690b786b802d8cb6cde67e42c23ef4ddc
Norah <alnoor0416@gmail.com>
2018-02-07 16:09:09 +0300

    wo

---
+++ Assets/worlds/settingManger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class settingManger : MonoBehaviour {
+
+	public void ContactUs()
+	{
+		string email = "renadalmalki2@gmail.com"; 
+		string title = emailURL ("FEEDBACK"); 
+		string msg = emailURL ("Please enter your message");
+
+		Application.OpenURL ("mailto:" + email + "?subject=" + title + "&body=" + msg); //open iphone mail 
+	}
+
+	string emailURL(string url){
+
+		return WWW.EscapeURL (url).Replace ("+", "%20");
+	}
+	 }
+
