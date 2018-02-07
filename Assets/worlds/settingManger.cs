fe207ed637da85de5e3830c8ef4d66e658fa8f77
renadsultan <32498605+renadsultan@users.noreply.github.com>
2018-02-07 16:22:41 +0300

    ث

--- Assets/worlds/settingManger.cs
+++ Assets/worlds/settingManger.cs
@@ -7,13 +7,13 @@ public class settingManger : MonoBehaviour {
 	public void ContactUs()
 	{
 		string email = "renadalmalki2@gmail.com"; 
-		string title = GoURL ("FEEDBACK"); 
-		string msg = GoURL ("Please enter your message");
+		string title = emailURL ("FEEDBACK"); 
+		string msg = emailURL ("Please enter your message");
 
 		Application.OpenURL ("mailto:" + email + "?subject=" + title + "&body=" + msg); //open iphone mail 
 	}
 
-	string GoURL(string url){
+	string emailURL(string url){
 
 		return WWW.EscapeURL (url).Replace ("+", "%20");
 	}
