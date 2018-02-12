b20741965e07b8b062fbf7e22e9f1a52e439ba13
Norah <alnoor0416@gmail.com>
2018-02-12 23:15:36 +0300

    the box finally there

--- Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARHitTestExample.cs
+++ Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARHitTestExample.cs
@@ -12,20 +12,20 @@ namespace UnityEngine.XR.iOS
 		public float maxRayDistance = 30.0f;
 		public LayerMask collisionLayer = 1 << 10;  //ARKitPlane layer
 		public bool isdetecting;
-        bool HitTestWithResultType (ARPoint point, ARHitTestResultType resultTypes)
-        {
-            List<ARHitTestResult> hitResults = UnityARSessionNativeInterface.GetARSessionNativeInterface ().HitTest (point, resultTypes);
-            if (hitResults.Count > 0) {
-                foreach (var hitResult in hitResults) {
-                    Debug.Log ("Got hit!");
-                    m_HitTransform.position = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
-                    m_HitTransform.rotation = UnityARMatrixOps.GetRotation (hitResult.worldTransform);
-                    Debug.Log (string.Format ("x:{0:0.######} y:{1:0.######} z:{2:0.######}", m_HitTransform.position.x, m_HitTransform.position.y, m_HitTransform.position.z));
-                    return true;
-                }
-            }
-            return false;
-        }
+		bool HitTestWithResultType (ARPoint point, ARHitTestResultType resultTypes)
+		{
+			List<ARHitTestResult> hitResults = UnityARSessionNativeInterface.GetARSessionNativeInterface ().HitTest (point, resultTypes);
+			if (hitResults.Count > 0) {
+				foreach (var hitResult in hitResults) {
+					Debug.Log ("Got hit!");
+					m_HitTransform.position = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
+					m_HitTransform.rotation = UnityARMatrixOps.GetRotation (hitResult.worldTransform);
+					Debug.Log (string.Format ("x:{0:0.######} y:{1:0.######} z:{2:0.######}", m_HitTransform.position.x, m_HitTransform.position.y, m_HitTransform.position.z));
+					return true;
+				}
+			}
+			return false;
+		}
 
 		// Update is called once per frame
 		void Update () {
@@ -64,22 +64,22 @@ namespace UnityEngine.XR.iOS
 						y = screenPosition.y
 					};
 
-                    // prioritize reults types
-                    ARHitTestResultType[] resultTypes = {
-                        ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent, 
-                        // if you want to use infinite planes use this:
-                        //ARHitTestResultType.ARHitTestResultTypeExistingPlane,
-                        ARHitTestResultType.ARHitTestResultTypeHorizontalPlane, 
-                        ARHitTestResultType.ARHitTestResultTypeFeaturePoint
-                    }; 
-					
-                    foreach (ARHitTestResultType resultType in resultTypes)
-                    {
-                        if (HitTestWithResultType (point, resultType))
-                        {
-                            return;
-                        }
-                    }
+					// prioritize reults types
+					ARHitTestResultType[] resultTypes = {
+						ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent, 
+						// if you want to use infinite planes use this:
+						//ARHitTestResultType.ARHitTestResultTypeExistingPlane,
+						ARHitTestResultType.ARHitTestResultTypeHorizontalPlane, 
+						ARHitTestResultType.ARHitTestResultTypeFeaturePoint
+					}; 
+
+					foreach (ARHitTestResultType resultType in resultTypes)
+					{
+						if (HitTestWithResultType (point, resultType))
+						{
+							return;
+						}
+					}
 				}
 			}
 			//#endif
@@ -87,7 +87,7 @@ namespace UnityEngine.XR.iOS
 
 		}
 
-	
+
 	}
 }
 
