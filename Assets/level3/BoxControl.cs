57c010149637c5db1c755f210aa9be8e937930e1
Norah <alnoor0416@gmail.com>
2018-02-16 18:35:54 +0300

    level 3
    some update in game logic and touch event

---
+++ Assets/level3/BoxControl.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class BoxControl: MonoBehaviour {
+
+	private Animation opining;
+	private bool isopen=false;
+	public static Ray ray;//this will be the ray that we cast from our touch into the scene
+	private static level3manger levelmanger; 
+
+
+	// Use this for initialization
+	void Start () {
+
+		opining = GetComponent<Animation>();
+		levelmanger = GetComponent<level3manger>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/*ray = Camera.main.ScreenPointToRay(_instance.position);//creates ray from screen point position
+		if (Physics.Raycast (ray,out hit,100)) {//+out hit 
+			print ("Hit box insaid!");
+					
+			if  (isopen)
+			{
+				closeit ();
+			} 
+			else
+			{
+				openit ();
+
+			}	
+		}*/
+
+
+		/*if(Physics.Raycast(transform.position, transform.forward,out hit, 2))
+		{
+			if(hit.collider.gameObject.tag == "main" )
+			{
+				var chest = gameObject.FindWithTag("chest");
+				if (Input.GetKeyDown("x"))
+				{
+					if  (isopen)
+					{
+						closeit ();
+					} 
+					else
+					{
+						openit ();
+
+					}
+				}
+			}
+		} */
+	}
+	public void openit(){
+		//animation.CrossFade ("walk");
+			opining.Play ("box_open");
+		    isopen = true;
+		levelmanger.CheckBoxes(this);
+
+	}
+	public void closeit(/* box number !*/){
+		opining.Play ("box_close");
+		//opining.Stop ();
+		isopen = false;
+
+	}
+	public void mached(){
+		//if get the cads match it 
+
+	}
+
+
+	public bool isOpen(){
+		return isopen;
+	}
+	public void setbumb(){
+	//StartCoroutine("Bumb");
+}
+	IEnumerator Bumb(){
+	yield return new WaitForSeconds(.5f);
+	//Destroy(gameObject);
+}
+}
--- Assets/level3/level3manger.cs
+++ Assets/level3/level3manger.cs
@@ -4,31 +4,77 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class level3manger : MonoBehaviour {
+
+	public List<GameObject> boxes = new List<GameObject>(); //array used to keep track of boxes objects
+	public int boxnum;//same as array of boxes/2 almost!
+	BoxControl current;
+	private BoxControl[] currentboxes = new BoxControl[2];
+	private int nroftries = 0;
 	// Use this for initialization
 	void Start () {
 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void touchsomething (GameObject hitobject) {
+		//check if its shadow or pairent don't get in 
+		//get it and openit or close it the mange will be in other method 
+		current = hitobject.GetComponent<BoxControl> ();
+		if (current!= null) {
+				if (current.isOpen ()) {
+					current.closeit ();
+				} else {
+					current.openit ();
+
+				}	
+			}else
+				print ("not box!");
+
+
 	}
 	public void home(){
-		SceneManager.LoadScene ("world");
+		//SceneManager.LoadScene ("world");
 
 
 	}
 
-	/*
-	public void open(){
+	//check if there is 2 opened before 
+	public void CheckBoxes(BoxControl bc){
+		if(currentboxes[0] == null)
+			currentboxes[0] = bc;
+		else{
+			currentboxes[1] = bc;
+			nroftries++;
+
+			if(currentboxes[0].boxnum == currentboxes[1].boxnum)
+				BoxesMatching();
+			else
+				BoxesNotMatching();
+
+			currentboxes[0] = null;
+			currentboxes[1] = null;
+		}
+	}
+	public void BoxesMatching(){
+		currentboxes[0].mached();
+		currentboxes[1].mached();
+
+		boxnum--;
+		if(boxnum == 0)
+			GameEnd();
+	}
+	public void BoxesNotMatching(){
+
+		currentboxes[0].closeit();
+		currentboxes[1].closeit();
+	}
+
+	public void timelimit(){
 
 
 	}
-	public void close(){
-
-
+	void GameEnd(){
+		//check wining or loosing
+		Debug.Log("Game has ended, number of tries: " + nroftries);
 	}
-	*/
 	public void save(){
 		PlayerPrefs.SetInt("level3Score",3);
 
