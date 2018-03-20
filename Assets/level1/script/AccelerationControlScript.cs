2061308f89f27934fc77a912ddf44d877e1aafe2
mac <noon251>
2018-03-20 14:03:53 +0300

    ff
    ff

--- Assets/level1/script/AccelerationControlScript.cs
+++ Assets/level1/script/AccelerationControlScript.cs
@@ -7,14 +7,25 @@ using UnityEngine.SceneManagement;
 public class AccelerationControlScript : MonoBehaviour {
 	public float keyDelay = 1f;
 	private float timePassed = 0;
-	public  float timer = 0;
+	public float timer;
 	public ThenderHide hide;
 	int count = 0;
 	string str = "grass";
 	public GameObject thenderHi;
 	public string temp;
 	Vector3 accelerationDir;
+	public int num=0;
+	public GameObject thender0;
+	public GameObject thender1;
+	public GameObject thender2;
+	public GameObject thender3;
+	public GameObject thender4;
+	public GameObject thender5;
+	public GameObject thender6;
+	public GameObject thender7;
+	public GameObject thender8;
 	public AccelerationControlScript script;
+	public GameObject grass0,grass1,grass3,grass8,grass9,grass11,grass13,grass15,grass17;
 	// Use this for initialization
 	void Start () {
 
@@ -23,39 +34,123 @@ public class AccelerationControlScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//read the shake of phone
-		accelerationDir = new Vector3(Input.acceleration.x,0,0);
+		accelerationDir = new Vector3 (Input.acceleration.x, 0, 0);
 		//this time betwwen two grass
 		timePassed += Time.deltaTime; 
-				timer+= Time.deltaTime; 
-			
+		//	timer+= Time.deltaTime; 
+
 		//to make sure the shake is happend
-		if (accelerationDir.sqrMagnitude >= 6f && timePassed >= keyDelay)
-		{			
+		if (accelerationDir.sqrMagnitude >= 6f && timePassed >= keyDelay) {			
 			//here to change the game object from counter 
 			temp = str + count;
 			//here to make grass hide
-			GameObject.Find(temp).GetComponent<Rigidbody2D> ().isKinematic = false;
-			GameObject.Find(temp).SetActive (false);
+			GameObject.Find (temp).GetComponent<Rigidbody2D> ().isKinematic = false;
+			GameObject.Find (temp).SetActive (false);
 			//reset the time passed to restart to another object 
-		
+			timer=4;
 			timePassed = 0;
-		
+			StartCoroutine(disable(num));
+
+			Debug.Log (num);
 			//here increes the counter to another object
 			count++;
 			Debug.Log (temp);
-//			if (timer >= 3) {
-//				Debug.Log (temp+"jj");
-//
-//				thenderHi.SetActive (true);
-//				script.enabled = false;
-//				timer = 0;
-//				Debug.Log (timer+"ss");
-//
-//			}
+			//			if (timer >= 3) {
+			//				Debug.Log (temp+"jj");
+			//
+			//				thenderHi.SetActive (true);
+			//				script.enabled = false;
+			//				timer = 0;
+			//				Debug.Log (timer+"ss");
+			//
+			//			}
 
 
 		}
 	}
+	IEnumerator disable(int num)
+	{ 				yield return new WaitForSeconds(timer);
+
+		if (num==0 && !grass0.activeInHierarchy) {
+			thender0.SetActive (false);//to disable thinder
+			Debug.Log (str + "dd");
+			num = 1;
+			timer = 0f;
+
+		}
+
+		if (num==1&&!grass1.activeInHierarchy) {
+			thender1.SetActive (false);//to disable thinder
+			Debug.Log (str + "dd");
+			num = 2;
+			timer = 0f;
+
+
+		}
+
+		if (num==2&&!grass3.activeInHierarchy) {
+			thender2.SetActive (false);//to disable thinder
+			Debug.Log (str + "dd");
+			num =3 ;
+
+			timer = 0f;
+
+		}
+
+		if (num==3&&!grass8.activeInHierarchy) {
+			thender3.SetActive (false);//to disable thinder
+			Debug.Log (str + "dd");
+			num =4 ;
+			timer = 0f;
+
+
+		}
+
+		if (num==4&&!grass9.activeInHierarchy) {
+			thender4.SetActive (false);//to disable thinder
+			thender5.SetActive (false);//to disable thinder
+			Debug.Log (str + "dd");
+			num =5 ;
+
+			timer = 0f;
+
+		}
+		if (num==5&&!grass11.activeInHierarchy) {
+			thender4.SetActive (false);//to disable thinder
+			thender5.SetActive (false);//to disable thinder
+			Debug.Log (str + "dd");
+			num =6 ;
+			timer = 0f;
+
+
+		}
+		if (num==6&&!grass13.activeInHierarchy) {
+			thender6.SetActive (false);//to disable thinder
+			Debug.Log (str + "dd");
+			num =7 ;
+			timer = 0f;
+
+
+		}
+		if (num==7&&!grass15.activeInHierarchy) {
+			thender7.SetActive (false);//to disable thinder
+			Debug.Log (str + "dd");
+			num =8;
+
+			timer = 0f;
+
+		}
+		if (num==8&&!grass17.activeInHierarchy) {
+			thender8.SetActive (false);//to disable thinder
+			Debug.Log (str + "dd");
+			num =9;
+			timer = 0f;
+
+
+		}
+
+	}
+
 }
-		
-	
+
+
