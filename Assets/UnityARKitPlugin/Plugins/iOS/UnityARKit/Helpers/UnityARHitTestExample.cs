40b0cb17bdeb6a377d3962349f26e24c2e6130e8
Mac <mac@Macs-MacBook.local>
2018-03-31 23:43:00 +0300

    .

--- Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARHitTestExample.cs
+++ Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARHitTestExample.cs
@@ -77,7 +77,7 @@ namespace UnityEngine.XR.iOS
 
 					distance=Vector3.Distance(item.transform.position,cam.transform.position);
 
-					if (distance < 5f) {
+					if (distance < 10f) {
 						levelmanager.touchsomething (item);
 					}
 
