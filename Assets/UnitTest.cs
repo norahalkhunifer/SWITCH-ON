a7ff3e51ce31222d05c3bffba0c897e8d578d57a
norah <alnoor0416@gmail.com>
2018-04-19 18:34:23 +0300

    testing

---
+++ Assets/UnitTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class UnitTest {
+
+	[Test]
+	public void UnitTestSimplePasses() {
+		// Use the Assert class to test conditions.
+		Assert.AreEqual(SetGetName.firstTime(),true);
+
+	}
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+
+	[UnityTest]
+	public IEnumerator UnitTestWithEnumeratorPasses() {
+		//var prefabs =
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+		Assert.AreEqual(SetGetName.firstTime(),true);
+
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SWITCH-ON
 switch on is fitness game for children made for King Saud University, the computer science college, software engineering department
+
+you need Xcode then 
